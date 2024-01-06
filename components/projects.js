1c02af0e819c65b7607e614d4b7fdc881824aeb9
Ahmet Can Doğan <ahmetcandgn@gmail.com>
2024-01-06 11:28:39 +0300

    New project added

--- components/projects.js
+++ components/projects.js
@@ -1,9 +1,5 @@
 import Image from "next/image";
 import Link from "next/link";
-import Card from "./ui/card";
-
-import container from "./ui/container";
-import Container from "./ui/container";
 
 export default function Projects() {
   return (
@@ -16,13 +12,76 @@ export default function Projects() {
       </p>
       <div className="space-y-8 w-full flex justify-center  items-center flex-col">
         <div className="p-4  rounded-lg bg-white drop-shadow-md flex  w-full max-w-6xl flex-col lg:flex-row">
-          <div className="relative w-full min-h-[300px] sm:min-h-[400px] border-gray-200 border rounded-lg">
+          <div className="relative w-full min-h-[250px] sm:min-h-[400px] border-gray-200 border rounded-lg">
+            <Image
+              src="/images/publishing_house.png"
+              fill
+              alt="Coffe Shop Project"
+              title="Coffe Shop Project"
+              className="rounded object-contain  "
+            />
+          </div>
+          <div className="flex flex-col items-center justify-center  text-center min-h-[400px] w-full">
+            <p className="text-h5 mb-2 uppercase font-bold text-gray-900">
+              {" "}
+              Headless Strapı CMS WIth Next.js (Multilang)
+            </p>
+            <p className="mb-2 max-w-sm text-gray-600 ">
+              This is a corporate website project for a publishing house.
+              Next.js was utilized for the frontend. It was internationalized
+              using the i18n library to support multiple languages. Strapi used
+              for CMS. All data was fetched using the REST API
+            </p>
+            <p className="text- font-medium mb-2 text-gray-600 ">Tools Used</p>
+            <div className="max-w-xs flex flex-wrap justify-center mb-4">
+              <p className="p-2.5 bg-gray-300 text-gray-600 font-medium leading-5 rounded m-1">
+                Figma
+              </p>
+              <p className="p-2.5 bg-gray-300 text-gray-600 font-medium leading-5 rounded m-1">
+                HTML
+              </p>
+              <p className="p-2.5 bg-gray-300 text-gray-600 font-medium leading-5 rounded m-1">
+                CSS
+              </p>
+              <p className="p-2.5 bg-gray-300 text-gray-600 font-medium leading-5 rounded m-1">
+                React
+              </p>
+              <p className="p-2.5 bg-gray-300 text-gray-600 font-medium leading-5 rounded m-1">
+                Next
+              </p>
+              <p className="p-2.5 bg-gray-300 text-gray-600 font-medium leading-5 rounded m-1">
+                Strapi
+              </p>
+              <p className="p-2.5 bg-gray-300 text-gray-600 font-medium leading-5 rounded m-1">
+                GIT
+              </p>
+            </div>
+            <div className="flex flex-wrap justify-center ">
+              <Link
+                href="https://github.com/ahmet-dgn/strapi-next-headless-cms.git"
+                className="p-2 font-medium leading-2 rounded border-2 border-custom-primary hover:bg-custom-primary  text-custom-primary hover:text-white m-1.5 block"
+                target="_blank"
+              >
+                GitHub
+              </Link>
+              <Link
+                href="https://headless-cms.ahmetcandgn.com"
+                className="p-2 font-medium leading-2 rounded border-2 border-custom-primary bg-custom-primary   text-white m-1.5 hover:bg-transparent hover:text-custom-primary block"
+                target="_blank"
+              >
+                Live Demo
+              </Link>
+            </div>
+          </div>
+        </div>
+        <div className="p-4  rounded-lg bg-white drop-shadow-md flex  w-full max-w-6xl flex-col lg:flex-row">
+          <div className="relative w-full min-h-[250px] sm:min-h-[400px] border-gray-200 border rounded-lg">
             <Image
               src="/images/figma-ui-kit.jpg"
               fill
               alt="Coffe Shop Project"
               title="Coffe Shop Project"
-              className="rounded object-cover  "
+              className="rounded object-contain  "
             />
           </div>
           <div className="flex flex-col items-center justify-center  text-center min-h-[400px] w-full">
@@ -55,38 +114,15 @@ export default function Projects() {
             </div>
           </div>
         </div>
+
         <div className="p-4  rounded-lg bg-white drop-shadow-md flex  w-full max-w-6xl flex-col lg:flex-row">
-          <div className="relative w-full min-h-[300px] sm:min-h-[400px] border-gray-200 border rounded-lg">
-            <Image
-              src="/images/publishing_house.png"
-              fill
-              alt="Coffe Shop Project"
-              title="Coffe Shop Project"
-              className="rounded object-cover  "
-            />
-          </div>
-          <div className="flex flex-col items-center justify-center  text-center min-h-[400px] w-full">
-            <p className="text-h5 mb-2 uppercase font-bold text-gray-900">
-              {" "}
-              Headless Wordpress CMS WIth Next.js
-            </p>
-            <p className="mb-6 max-w-sm text-gray-600 ">
-              I am currently working on a publishing house website. I use
-              Next.js on the frontend and headless WordPress for CMS.
-            </p>
-            <p className="mb-6 max-w-sm text-gray-600 ">
-              It will be available in a few weeks.
-            </p>
-          </div>
-        </div>
-        <div className="p-4  rounded-lg bg-white drop-shadow-md flex  w-full max-w-6xl flex-col lg:flex-row">
-          <div className="relative w-full min-h-[300px] sm:min-h-[400px]">
+          <div className="relative w-full min-h-[250px] sm:min-h-[400px]">
             <Image
               src="/images/coffeShop.png"
               fill
               alt="Coffe Shop Project"
               title="Coffe Shop Project"
-              className="rounded object-cover  "
+              className="rounded object-contain   "
             />
           </div>
           <div className="flex flex-col items-center justify-center  text-center min-h-[400px] w-full">
@@ -146,13 +182,13 @@ export default function Projects() {
           </div>
         </div>
         <div className="p-4  rounded-lg bg-white drop-shadow-md flex  w-full max-w-6xl flex-col lg:flex-row">
-          <div className="relative w-full  min-h-[300px] sm:min-h-[400px]">
+          <div className="relative w-full  min-h-[250px] sm:min-h-[400px]">
             <Image
               src="/images/expenseTracker.png"
               fill
               alt="Expense Tracker Project"
               title="Expense Tracker Project"
-              className="rounded object-cover "
+              className="rounded object-contain  "
             />
           </div>
           <div className="flex flex-col items-center justify-center  text-center min-h-[400px] w-full">
