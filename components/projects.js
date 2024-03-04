f655d12e72f45418e34f24e6b7fc7237c02708a8
Ahmet <ahmet@192.168.1.108>
2024-03-04 23:36:48 +0300

    design update

--- components/projects.js
+++ components/projects.js
@@ -181,68 +181,6 @@ export default function Projects() {
             </div>
           </div>
         </div>
-        <div className="p-4  rounded-lg bg-white drop-shadow-md flex  w-full max-w-6xl flex-col lg:flex-row">
-          <div className="relative w-full  min-h-[250px] sm:min-h-[400px]">
-            <Image
-              src="/images/expenseTracker.png"
-              fill
-              alt="Expense Tracker Project"
-              title="Expense Tracker Project"
-              className="rounded object-contain  "
-            />
-          </div>
-          <div className="flex flex-col items-center justify-center  text-center min-h-[400px] w-full">
-            <p className="text-h5 mb-2 uppercase font-bold text-gray-900 ">
-              {" "}
-              EXPENSE TRACKER
-            </p>
-            <p className="mb-6 max-w-sm text-gray-600 ">
-              I only used react in this application. I also designed the app in
-              Figma.
-            </p>
-            <p className="text-gray-600 font-medium mb-2 ">Tools Used</p>
-            <div className="max-w-xs flex flex-wrap justify-center mb-9">
-              <p className="p-2.5 bg-gray-300 text-gray-600  font-medium leading-5 rounded m-1">
-                Figma
-              </p>
-              <p className="p-2.5 bg-gray-300 text-gray-600 font-medium leading-5 rounded m-1">
-                HTML
-              </p>
-              <p className="p-2.5 bg-gray-300 text-gray-600  font-medium leading-5 rounded m-1">
-                CSS
-              </p>
-              <p className="p-2.5 bg-gray-300 text-gray-600  font-medium leading-5 rounded m-1">
-                React
-              </p>
-              <p className="p-2.5 bg-gray-300 text-gray-600  font-medium leading-5 rounded m-1">
-                GIT
-              </p>
-            </div>
-            <div className="flex flex-wrap justify-center ">
-              <Link
-                href="https://www.figma.com/file/N5p5VO6j453W2EttcuqaE6/Expenses?type=design&mode=design"
-                className="p-2 font-medium leading-2 rounded border-2 border-custom-primary hover:bg-custom-primary  text-custom-primary hover:text-white m-1.5 block"
-                target="_blank"
-              >
-                Figma
-              </Link>
-              <Link
-                href="https://github.com/ahmet-dgn/expense-tracker"
-                className="p-2 font-medium leading-2 rounded border-2 border-custom-primary hover:bg-custom-primary  text-custom-primary hover:text-white m-1.5 block"
-                target="_blank"
-              >
-                GitHub
-              </Link>
-              <Link
-                href="https://expense-tracker.ahmetcandgn.com/"
-                className="p-2 font-medium leading-2 rounded border-2 border-custom-primary bg-custom-primary  text-white m-1.5 hover:bg-transparent hover:text-custom-primary block"
-                target="_blank"
-              >
-                Live Demo
-              </Link>
-            </div>
-          </div>
-        </div>
       </div>
     </div>
   );
